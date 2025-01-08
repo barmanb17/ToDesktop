1d1b86498880f4af46959d1f9f3c076187dc1a34
barmanb17 <bijoy20-127@aec.ac.in>
2025-01-08 05:51:36 +0530

    #howitworks

--- src/components/HowItWorks.jsx
+++ src/components/HowItWorks.jsx
@@ -1,8 +1,159 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheck } from '@fortawesome/free-solid-svg-icons'
 import React from 'react'
 
 const HowItWorks = () => {
   return (
-    <div>HowItWorks</div>
+    // Step 1
+    <div className='flex flex-col gap-6 px-6 py-12 max-w-7xl mt-16 mx-auto lg:px-8 lg:mt-32 overflow-hidden'>
+      <h2 className='font-display text-5xl sm:font-semibold mb-14'>How it works</h2>
+      <div className='rounded-xl border-2 px-8 py-12 flex flex-col lg:flex-row gap-6'>
+        {/* left part */}
+        <div className='flex flex-col gap-6 lg:w-1/2'>
+          <span className='border-2 border-yellow-300 bg-yellow-50 text-yellow-800 w-fit px-3 py-1 rounded-lg font-medium font-display'>Step 1</span>
+          <h3 className='text-4xl'>Bootstrap straight from your web app</h3>
+          <p className='text-lg font-light'>Copy and paste your web app url into ToDesktop. Customise your app design, app icon and window frame UI with no code.</p>
+            {/* list */}
+           
+           <ul className='grid grid-cols-2 gap-2'>
+             <li className='flex gap-4 items-center'>
+               <FontAwesomeIcon icon={faCheck} />
+               <a className='text-lg font-light border-b-2 border-gray-600 border-opacity-30 hover:border-opacity-80' href="#">Multiple windows</a>
+             </li>
+
+             <li className='flex gap-4 items-center'>
+               <FontAwesomeIcon icon={faCheck} />
+               <a className='text-lg font-light border-b-2 border-gray-600 border-opacity-30 hover:border-opacity-80' href="#">Multiple windows</a>
+             </li>
+
+             <li className='flex gap-4 items-center'>
+               <FontAwesomeIcon icon={faCheck} />
+               <a className='text-lg font-light border-b-2 border-gray-600 border-opacity-30 hover:border-opacity-80' href="#">Multiple windows</a>
+             </li>
+
+             <li className='flex gap-4 items-center'>
+               <FontAwesomeIcon icon={faCheck} />
+               <a className='text-lg font-light border-b-2 border-gray-600 border-opacity-30 hover:border-opacity-80' href="#">Multiple windows</a>
+             </li>
+
+             <li className='flex gap-4 items-center'>
+               <FontAwesomeIcon icon={faCheck} />
+               <a className='text-lg font-light border-b-2 border-gray-600 border-opacity-30 hover:border-opacity-80' href="#">Multiple windows</a>
+             </li>
+
+             <li className='flex gap-4 items-center'>
+               <FontAwesomeIcon icon={faCheck} />
+               <a className='text-lg font-light border-b-2 border-gray-600 border-opacity-30 hover:border-opacity-80' href="#">Multiple windows</a>
+             </li>
+           </ul>
+
+        </div>
+        {/* Right part */}
+
+        <div className='pt-12 '>
+          <img src="/src/assets/asset 66.svg" alt="" />
+        </div>
+      </div>
+      {/* Step2 */}
+
+      <div className='rounded-xl border-2 px-8 py-12 flex flex-col lg:flex-row gap-6'>
+        {/* left part */}
+        <div className='flex flex-col gap-6 lg:w-1/2'>
+          <span className='border-2 border-yellow-300 bg-yellow-50 text-yellow-800 w-fit px-3 py-1 rounded-lg font-medium font-display'>Step 2</span>
+          <h3 className='text-4xl'>Bootstrap straight from your web app</h3>
+          <p className='text-lg font-light'>Copy and paste your web app url into ToDesktop. Customise your app design, app icon and window frame UI with no code.</p>
+            {/* list */}
+           
+           <ul className='grid grid-cols-2 gap-2'>
+             <li className='flex gap-4 items-center'>
+               <FontAwesomeIcon icon={faCheck} />
+               <a className='text-lg font-light border-b-2 border-gray-600 border-opacity-30 hover:border-opacity-80' href="#">Multiple windows</a>
+             </li>
+
+             <li className='flex gap-4 items-center'>
+               <FontAwesomeIcon icon={faCheck} />
+               <a className='text-lg font-light border-b-2 border-gray-600 border-opacity-30 hover:border-opacity-80' href="#">Multiple windows</a>
+             </li>
+
+             <li className='flex gap-4 items-center'>
+               <FontAwesomeIcon icon={faCheck} />
+               <a className='text-lg font-light border-b-2 border-gray-600 border-opacity-30 hover:border-opacity-80' href="#">Multiple windows</a>
+             </li>
+
+             <li className='flex gap-4 items-center'>
+               <FontAwesomeIcon icon={faCheck} />
+               <a className='text-lg font-light border-b-2 border-gray-600 border-opacity-30 hover:border-opacity-80' href="#">Multiple windows</a>
+             </li>
+
+             <li className='flex gap-4 items-center'>
+               <FontAwesomeIcon icon={faCheck} />
+               <a className='text-lg font-light border-b-2 border-gray-600 border-opacity-30 hover:border-opacity-80' href="#">Multiple windows</a>
+             </li>
+
+             <li className='flex gap-4 items-center'>
+               <FontAwesomeIcon icon={faCheck} />
+               <a className='text-lg font-light border-b-2 border-gray-600 border-opacity-30 hover:border-opacity-80' href="#">Multiple windows</a>
+             </li>
+           </ul>
+
+        </div>
+        {/* Right part */}
+
+        <div className='pt-12 '>
+          <img src="/src/assets/asset 66.svg" alt="" />
+        </div>
+      </div>
+
+      {/* step3 */}
+
+      <div className='rounded-xl border-2 px-8 py-12 flex flex-col lg:flex-row gap-6'>
+        {/* left part */}
+        <div className='flex flex-col gap-6 lg:w-1/2'>
+          <span className='border-2 border-yellow-300 bg-yellow-50 text-yellow-800 w-fit px-3 py-1 rounded-lg font-medium font-display'>Step 3</span>
+          <h3 className='text-4xl'>Bootstrap straight from your web app</h3>
+          <p className='text-lg font-light'>Copy and paste your web app url into ToDesktop. Customise your app design, app icon and window frame UI with no code.</p>
+            {/* list */}
+           
+           <ul className='grid grid-cols-2 gap-2'>
+             <li className='flex gap-4 items-center'>
+               <FontAwesomeIcon icon={faCheck} />
+               <a className='text-lg font-light border-b-2 border-gray-600 border-opacity-30 hover:border-opacity-80' href="#">Multiple windows</a>
+             </li>
+
+             <li className='flex gap-4 items-center'>
+               <FontAwesomeIcon icon={faCheck} />
+               <a className='text-lg font-light border-b-2 border-gray-600 border-opacity-30 hover:border-opacity-80' href="#">Multiple windows</a>
+             </li>
+
+             <li className='flex gap-4 items-center'>
+               <FontAwesomeIcon icon={faCheck} />
+               <a className='text-lg font-light border-b-2 border-gray-600 border-opacity-30 hover:border-opacity-80' href="#">Multiple windows</a>
+             </li>
+
+             <li className='flex gap-4 items-center'>
+               <FontAwesomeIcon icon={faCheck} />
+               <a className='text-lg font-light border-b-2 border-gray-600 border-opacity-30 hover:border-opacity-80' href="#">Multiple windows</a>
+             </li>
+
+             <li className='flex gap-4 items-center'>
+               <FontAwesomeIcon icon={faCheck} />
+               <a className='text-lg font-light border-b-2 border-gray-600 border-opacity-30 hover:border-opacity-80' href="#">Multiple windows</a>
+             </li>
+
+             <li className='flex gap-4 items-center'>
+               <FontAwesomeIcon icon={faCheck} />
+               <a className='text-lg font-light border-b-2 border-gray-600 border-opacity-30 hover:border-opacity-80' href="#">Multiple windows</a>
+             </li>
+           </ul>
+
+        </div>
+        {/* Right part */}
+
+        <div className='pt-12 '>
+          <img src="/src/assets/asset 66.svg" alt="" />
+        </div>
+      </div>
+    </div>
   )
 }
 
